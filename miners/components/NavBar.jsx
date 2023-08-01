8a72ea1e1422a6a291a33624ada57898988d60a9
ReturnerNaGithub <damsan1@gmail.com>
2023-08-01 23:28:22 +0200

    miners

--- miners/components/NavBar.jsx
+++ miners/components/NavBar.jsx
@@ -43,23 +43,23 @@ function NavBar() {
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
                 <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#about" onClick={() => setNavbar(!navbar)}>
-                    About
+                  <Link href="#Game" onClick={() => setNavbar(!navbar)}>
+                    Your Games
                   </Link>
                 </li>
                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#blog" onClick={() => setNavbar(!navbar)}>
-                    Blogs
+                  <Link href="#Tokens" onClick={() => setNavbar(!navbar)}>
+                    Your tokens
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#Future" onClick={() => setNavbar(!navbar)}>
+                    Your Future
                   </Link>
                 </li>
                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="#contact" onClick={() => setNavbar(!navbar)}>
                     Contact
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#projects" onClick={() => setNavbar(!navbar)}>
-                    Projects
                   </Link>
                 </li>
               </ul>
--- miners/pages/index.js
+++ miners/pages/index.js
@@ -5,16 +5,25 @@ export default function Home() {
     <div>
       <NavBar />
       <section
-        id="about"
+        id="Game"
         className="text-white h-screen bg-black  text-center flex"
       >
         <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold  ">
-          About
+          Your Games
         </h1>
       </section>
-      <section id="blog" className=" text-white h-screen bg-purple-900 flex">
+      <section id="Tokens" className=" text-white h-screen bg-purple-900 flex">
         <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
-          Blog
+          Your Tokens
+        </h1>
+      </section>
+      
+      <section
+        id="Future"
+        className=" text-white h-screen bg-purple-900 flex"
+      >
+        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
+          Your Future
         </h1>
       </section>
       <section
@@ -23,14 +32,6 @@ export default function Home() {
       >
         <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">
           Contact{' '}
-        </h1>
-      </section>
-      <section
-        id="projects"
-        className=" text-white h-screen bg-purple-900 flex"
-      >
-        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
-          Projects
         </h1>
       </section>
     </div>
